050daec2a4350b0a36d9e70e88585043ce1474e7
Yuvraj Grover <yuvrajgrover6@gmail.com>
2025-07-02 17:35:37 -0400

    Update YOLO model configuration and training parameters for improved performance

--- model/train.py
+++ model/train.py
@@ -1,17 +1,21 @@
 # train.py — YOLOv8 training script
 
+from functools import cache
 from ultralytics import YOLO
 
 # Load model (choose yolov8n.yaml or yolov8s.yaml based on performance vs speed)
-model = YOLO('yolov8n.yaml')  # or yolov8s.yaml
+model = YOLO('yolov5n.yaml')  # or yolov8s.yaml
 
 # Train
 model.train(
     data='dataset/waste_dataset.yaml',
-    imgsz=640,
-    epochs=50,
-    batch=16,
-    device='mps',  # 'cuda' for GPU, 'cpu' otherwise
-    name='waste_yolov8_model',
-    pretrained=True
+    imgsz=320,
+    epochs=5,
+    batch=32,
+    device='mps',
+    name='debug_yolov8',
+    pretrained=True,
+    cache='disk'  # avoids RAM overload
 )
+
+
